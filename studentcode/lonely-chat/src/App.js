563954bd929ddf90c1dbfcdc86b44b26c9551f03
Richard Bonneau <richardbonneau@hotmail.com>
2018-04-21 21:15:32 -0400

    2018-04-21

--- studentcode/lonely-chat/src/App.js
+++ studentcode/lonely-chat/src/App.js
@@ -5,43 +5,113 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      messageList: [],
-      inputText: '',
-      update: ''
+      messageListEmilie: [],
+      messageListJacques: [],
+      inputTextEmilie: '',
+      inputTextJacques: '',
+      username: '',
+      inputUsername: '',
+      renderUsernamePick: true,
+      renderChatroom: false,
+      room: 'none'
     }
   }
- handleSubmit = (event) => {
+  
+  //  Bot Code Em
+ handleSubmitEmilie = (event) => {
    event.preventDefault();
-   this.state.messageList.push("Richard: " + this.state.inputText);
+   this.state.messageListEmilie.push(this.state.username + ": " + this.state.inputTextEmilie);
    setTimeout(()=> {
      let botPossibleAnswers = ["Interesting.", "Oh that's cool", "Alright."];
-     this.state.messageList.push("Emilie: " + botPossibleAnswers[Math.floor(Math.random()*3)]);
-     this.setState({update: 0});
+     this.state.messageListEmilie.push("Emilie: " + botPossibleAnswers[Math.floor(Math.random()*3)]);
+     this.setState({});
     }, 1000)
-    setTimeout(()=> {
-      let botPossibleAnswers = ["HEY!.", "Don't forget about me!", "Why do you only care about Emilie? :("];
-      this.state.messageList.push("All of my other friends: " + botPossibleAnswers[Math.floor(Math.random()*3)]);
-      this.setState({update: 0});
-     }, 1500)
-   this.setState({inputText: ''});
+   this.setState({inputTextEmilie: ''});
  }
- handleChange = (event) => {
-   this.setState({inputText: event.target.value});
+ handleChangeEmilie = (event) => {
+   this.setState({inputTextEmilie: event.target.value});
  }
-
-
+ // Bot code Jacques
+ handleSubmitJacques = (event) => {
+  event.preventDefault();
+  this.state.messageListJacques.push(this.state.username + ": " + this.state.inputTextJacques);
+  setTimeout(()=> {
+    let botPossibleAnswers = [
+"Coding. Robot. Javascript. Beep.", "Click here if you are not a robot", "I eat and drink like a human"];
+    this.state.messageListJacques.push("Jacques: " + botPossibleAnswers[Math.floor(Math.random()*3)]);
+    this.setState({});
+   }, 1000)
+  this.setState({inputTextJacques: ''});
+}
+handleChangeJacques = (event) => {
+  this.setState({inputTextJacques: event.target.value});
+}
+ // Submit username Code
+ handleSubmitUsername = (event) => {
+   event.preventDefault();
+   this.setState({username: this.state.inputUsername});
+   this.setState({renderUsernamePick: false})
+ }
+ handleChangeUsername= (event) => {
+   this.setState({inputUsername: event.target.value});
+ }
+ // Creating the individual chatrooms
+ openEmilieChat = () => {
+    this.setState({room: "em"})
+ }
+ openJacquesChat = () => {
+      this.setState({room: "jac"})
+ }
   render() {
     return(
-      <div className="App">
-      <form onSubmit={this.handleSubmit}>
-      <ul className='App'>
-      {this.state.messageList.map((msg)=> <li>{msg}</li>)}        
-      </ul>
-        <input type='text' value={this.state.inputText} onChange={this.handleChange} />
+<div className="App">
+{
+  this.state.renderUsernamePick ?
+      <form style={{textAlign: 'center'}} onSubmit={this.handleSubmitUsername}>
+      <h1>Pick a username!</h1>
+        <input type='text' value={this.state.inputUsername} onChange={this.handleChangeUsername} />
         <input type='submit' value='submit' />
         </form>
+        : 
         
-      </div>
+  <div style={{display: 'flex'}}>
+
+        <ul style={{flexGrow:'.2'}} className="App">
+        <li>
+        <a href="#" onClick={this.openEmilieChat}>Emilie Chatroom</a>
+        </li>
+        <a href="#" onClick={this.openJacquesChat}>Jacques Chatroom</a>
+        </ul>
+
+    <div style={{flexGrow:'0.2'}} />
+
+      <div style={{flexGrow:'1'}}>
+      {
+        this.state.room === 'none' ? <div /> :
+        this.state.room === "em" ?
+        <form onSubmit={this.handleSubmitEmilie}>
+        <h2>Thank you {this.state.username}. You may start chatting with Emilie!</h2>
+        <ul className='App'>
+        {this.state.messageListEmilie.map((msg)=> <li>{msg}</li>)}        
+        </ul>
+          <input type='text' value={this.state.inputTextEmilie} onChange={this.handleChangeEmilie} />
+          <input type='submit' value='submit' />
+          </form> 
+          :  
+          <form onSubmit={this.handleSubmitJacques}>
+    <h2>Thank you {this.state.username}. You may start chatting with Jacques!</h2>
+    <ul className='App'>
+    {this.state.messageListJacques.map((msg)=> <li>{msg}</li>)}        
+    </ul>
+      <input type='text' value={this.state.inputTextJacques} onChange={this.handleChangeJacques} />
+      <input type='submit' value='submit' />
+      </form>
+      }
+</div>
+
+  </div>
+}
+</div>
     )
   }
 }
