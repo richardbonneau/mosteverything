a5733cf1cc5f8ff436d04cc4d0077fe847321eb1
Richard Bonneau <richardbonneau@hotmail.com>
2018-04-22 21:35:20 -0400

    22 avril

--- studentcode/react-form/src/App.js
+++ studentcode/react-form/src/App.js
@@ -2,20 +2,73 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      firstNameValue: '',
+      lastNameValue: ''
+  };
+  }
+  isFirstNameInputPopulated = false;
+  isLastNameInputPopulated = false;
+ 
+  handleChangeFirstName = (event) => {
+    this.setState({firstNameValue: event.target.value});
+    if(event.target.value !== '') {
+      this.isFirstNameInputPopulated = true;
+    } else this.isFirstNameInputPopulated = false;
+  }
+  handleChangeLastName = (event) => {
+    this.setState({lastNameValue: event.target.value});
+    if(event.target.value !== '') {
+      this.isLastNameInputPopulated = true;
+    } else this.isLastNameInputPopulated = false;
+
+  }
+  handleSubmit = (event) => {
+    alert('Hi ' + this.state.firstNameValue + ' ' + this.state.lastNameValue);
+    event.preventDefault();
+  }
+  clearFields = () => {
+    this.setState({
+      firstNameValue: '',
+      lastNameValue: ''
+  })
+  this.isFirstNameInputPopulated = false;
+  this.isLastNameInputPopulated = false;
+  }
+  swapFields = () => {
+    this.setState({
+      firstNameValue: this.state.lastNameValue,
+      lastNameValue: this.state.firstNameValue
+    
+    })
+  }
+ 
+  render = () => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      <form onSubmit={this.handleSubmit}>
+      <div>
+          First Name:
+          <input type="text" value={this.state.firstNameValue} onChange={this.handleChangeFirstName} />
+          </div>
+          <div>
+          Last Name:
+          <input type="text" value={this.state.lastNameValue} onChange={this.handleChangeLastName} />
+          </div>
+          {
+            this.isFirstNameInputPopulated && this.isLastNameInputPopulated ? 
+          <input type="submit" value="Submit" />
+          : <div />
+          }
+      </form>
+      <button onClick={this.clearFields}>Clear</button>
+      <button onClick={this.swapFields}>Swap</button>
       </div>
     );
   }
-}
+ }
 
 export default App;
